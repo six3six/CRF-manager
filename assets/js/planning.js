8e2ea2ef6dcce5cf1e9641bc6546346ef44c2d2f
Louis Desplanche <l.desplanche@gmail.com>
2020-04-08 15:35:31 +0200

    Add source fetching (planning)

--- assets/js/planning.js
+++ assets/js/planning.js
@@ -2,24 +2,40 @@ import '../css/planning.scss';
 import '@fullcalendar/core/main.css';
 import '@fullcalendar/daygrid/main.css';
 
-import { Calendar } from '@fullcalendar/core';
+import {Calendar} from '@fullcalendar/core';
 import dayGridPlugin from '@fullcalendar/daygrid';
 import interactionPlugin from '@fullcalendar/interaction';
 
 import frLocale from '@fullcalendar/core/locales/fr';
 
 import $ from 'jquery';
+
+let dataSource = "/planning/source/";
 $(function () {
     let calendarEl = $("#calendar")[0];
     let calendar = new Calendar(calendarEl, {
         locale: frLocale,
-        plugins: [ dayGridPlugin, interactionPlugin ],
+        plugins: [dayGridPlugin, interactionPlugin],
         defaultView: 'dayGridMonth',
         selectable: true,
-        dateClick: function(info) {
+        eventSources: [
+            {
+                url: dataSource + "availabilities",
+                color: 'blue',   // a non-ajax option
+            },
+            {
+                url: dataSource + "events",
+                color: 'red',   // a non-ajax option
+            },
+            {
+                url: dataSource + "tests",
+                color: 'yellow',   // a non-ajax option
+            }
+        ],
+        dateClick: function (info) {
             alert('clicked ' + info.dateStr);
         },
-        select: function(info) {
+        select: function (info) {
             alert('selected ' + info.startStr + ' to ' + info.endStr);
         }
     });
--- src/Controller/PlanningController.php
+++ src/Controller/PlanningController.php
@@ -2,7 +2,14 @@
 
 namespace App\Controller;
 
+
+use App\Entity\Availability;
+use App\Entity\Event;
+use App\Entity\User;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Symfony\Component\Routing\Annotation\Route;
 
 class PlanningController extends AbstractController
@@ -15,5 +22,46 @@ class PlanningController extends AbstractController
         return $this->render('planning/index.html.twig', [
             'controller_name' => 'PlanningController',
         ]);
+    }
+
+    /**
+     * @Route("/planning/source/{source}", name="planningSources")
+     */
+    public function source($source)
+    {
+        $calendar = array();
+        $user = $this->getUser();
+
+        $events = $user->getEvents();
+        $availabilities = $user->getAvailabilities();
+        if ($source == "events") {
+            foreach ($events as $event) {
+                $f_event = array(
+                    "title" => $event->getName(),
+                    "start" => $event->getStart()->format(\DateTime::ISO8601),
+                    "end" => $event->getStop()->format(\DateTime::ISO8601),
+                );
+                array_push($calendar, $f_event);
+            }
+        } elseif ($source == "availabilities") {
+            foreach ($availabilities as $availability) {
+                $f_event = array(
+                    "title" => $availability->getName(),
+                    "start" => $availability->getStart()->format(\DateTime::ISO8601),
+                    "end" => $availability->getStop()->format(\DateTime::ISO8601),
+                );
+                array_push($calendar, $f_event);
+            }
+        } elseif ($source == "tests") {
+            array_push($calendar, array(
+                "title" => "Test",
+                "start" => (new \DateTime())->format(\DateTime::ISO8601),
+                "end" => (new \DateTime("2020-04-10"))->format(\DateTime::ISO8601)
+            ));
+        } else {
+            throw new NotFoundHttpException("Source not found");
+        }
+
+        return new JsonResponse($calendar);
     }
 }
