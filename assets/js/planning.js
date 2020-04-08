dc943589e120b25d31ce3e2386119cee98411646
Louis Desplanche <l.desplanche@gmail.com>
2020-04-08 10:57:26 +0200

    Login page creation

--- assets/js/planning.js
+++ assets/js/planning.js
@@ -4,17 +4,26 @@ import '@fullcalendar/daygrid/main.css';
 
 import { Calendar } from '@fullcalendar/core';
 import dayGridPlugin from '@fullcalendar/daygrid';
+import interactionPlugin from '@fullcalendar/interaction';
+
+import frLocale from '@fullcalendar/core/locales/fr';
 
 import $ from 'jquery';
 $(function () {
     let calendarEl = $("#calendar")[0];
     let calendar = new Calendar(calendarEl, {
-        plugins: [ dayGridPlugin ],
+        locale: frLocale,
+        plugins: [ dayGridPlugin, interactionPlugin ],
         defaultView: 'dayGridMonth',
-        selectable: true
+        selectable: true,
+        dateClick: function(info) {
+            alert('clicked ' + info.dateStr);
+        },
+        select: function(info) {
+            alert('selected ' + info.startStr + ' to ' + info.endStr);
+        }
     });
     calendar.render();
-    console.log(calendar);
 });
 
 
