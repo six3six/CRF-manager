a794e01b0be59dcd9bff864420bfb91b9e6b7ab9
Louis Desplanche <l.desplanche@gmail.com>
2020-04-09 16:07:59 +0200

    Add profil page

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -3,9 +3,14 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\CssSelector\Exception\InternalErrorException;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Exception\UnauthorizedHttpException;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+use Symfony\Component\HttpFoundation\Request;
 
 class SecurityController extends AbstractController
 {
@@ -17,7 +22,7 @@ class SecurityController extends AbstractController
     public function login(AuthenticationUtils $authenticationUtils): Response
     {
         if ($this->getUser()) {
-             return $this->redirectToRoute('planning');
+            return $this->redirectToRoute('planning');
         }
 
         // get the login error if there is one
@@ -26,6 +31,48 @@ class SecurityController extends AbstractController
         $lastUsername = $authenticationUtils->getLastUsername();
 
         return $this->render('security/login.html.twig', ['last_username' => $lastUsername, 'error' => $error]);
+    }
+
+    /**
+     * @Route("/profil", name="app_profil")
+     */
+    public function profil()
+    {
+        return $this->render("security/profil.html.twig", ["error" => ""]);
+    }
+
+    /**
+     * @Route("/email", name="app_chgemail", methods={"POST"})
+     * @param Request $r
+     * @return RedirectResponse
+     */
+    public function email(Request $r)
+    {
+        $this->getUser()->setEmail($r->request->get("email"));
+        $this->getDoctrine()->getManager()->persist($this->getUser());
+        $this->getDoctrine()->getManager()->flush();
+        return new RedirectResponse("/profil");
+    }
+
+    /**
+     * @Route("/password", name="app_chgpassword", methods={"POST"})
+     * @param Request $r
+     * @param UserPasswordEncoderInterface $passwordEncoder
+     * @return RedirectResponse
+     * @throws InternalErrorException
+     */
+    public function password(Request $r, UserPasswordEncoderInterface $passwordEncoder)
+    {
+        $anc = $r->request->get("ancPassword");
+
+        if ($r->request->get("newPassword") !== $r->request->get("confNewPassword")) throw new InternalErrorException("Les mots de passes ne sont pas identiques");
+
+        if (!$passwordEncoder->isPasswordValid($this->getUser(), $anc)) throw new UnauthorizedHttpException("", "Ancien mot de passe invalide");
+
+        $this->getUser()->setPassword($passwordEncoder->encodePassword($this->getUser(), $r->request->get("newPassword")));
+        $this->getDoctrine()->getManager()->persist($this->getUser());
+        $this->getDoctrine()->getManager()->flush();
+        return new RedirectResponse("/profil");
     }
 
     /**
