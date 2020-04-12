b09148f21b3a16ade92308661cdc404f8269eb97
Louis DESPLANCHE <louis.desplanche@edu.esiee.fr>
2020-04-12 15:30:17 +0200

    Add admin user editing

--- assets/js/planning.js
+++ assets/js/planning.js
@@ -1,16 +1,19 @@
 import '../css/planning.scss';
+
 require("core-js");
 
 import {Calendar} from '@fullcalendar/core';
 import dayGridPlugin from '@fullcalendar/daygrid';
 import timeGridPlugin from '@fullcalendar/timegrid';
 import interactionPlugin from '@fullcalendar/interaction';
+import listPlugin from '@fullcalendar/list';
 
 import frLocale from '@fullcalendar/core/locales/fr';
 
 import '@fullcalendar/core/main.css';
 import '@fullcalendar/daygrid/main.css';
 import '@fullcalendar/timegrid/main.css';
+import '@fullcalendar/list/main.css';
 
 import $ from 'jquery';
 
@@ -22,14 +25,14 @@ $(function () {
     let calendarEl = $("#calendar")[0];
     let calendar = new Calendar(calendarEl, {
         locale: frLocale,
-        plugins: [dayGridPlugin, interactionPlugin, timeGridPlugin],
+        plugins: [dayGridPlugin, interactionPlugin, timeGridPlugin, listPlugin],
         defaultView: 'dayGridMonth',
         selectable: true,
         timezone: 'CET',
         header: {
             left: 'prev,next today',
             center: 'title',
-            right: 'dayGridMonth,timeGridWeek'
+            right: 'dayGridMonth,timeGridWeek,listWeek'
         },
         eventSources: [
             {
--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -5,6 +5,8 @@ namespace App\Controller;
 use App\Entity\User;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -48,6 +50,62 @@ class   AdminController extends AbstractController
             'controller_name' => 'AdminController',
             "users" => $all_users,
         ]);
+    }
+
+    /**
+     * @Route("/admin/user_edit/{username}", name="admin_user_edit", methods={"GET"})
+     * @param $username
+     * @return Response
+     */
+    public function user_edit($username, $error = "")
+    {
+        $userRepo = $this->getDoctrine()->getRepository(User::class);
+        $user = $userRepo->findOneBy(["username" => $username]);
+        if ($user == null) throw new NotFoundHttpException("Utilisateur non trouvé");
+        return $this->render('admin/user_edit.html.twig', [
+            'controller_name' => 'AdminController',
+            "user" => $user,
+            "error" => $error
+        ]);
+    }
+
+    /**
+     * @Route("/admin/user_edit/{username}", name="admin_user_edit_save", methods={"POST"})
+     * @param Request $request
+     * @param $username
+     * @return Response
+     */
+    public function user_edit_save(Request $request, $username)
+    {
+        $userRepo = $this->getDoctrine()->getRepository(User::class);
+        $user = $userRepo->findOneBy(["username" => $username]);
+        if ($user == null) throw new NotFoundHttpException("Utilisateur non trouvé");
+
+        $keys = $request->request->keys();
+        $error = "";
+        $error .= !in_array("address", $keys) ? "Il manque le champ adresse<br/>" : "";
+        $error .= !in_array("email", $keys) ? "Il manque le champ email<br/>" : "";
+        $error .= !in_array("first_name", $keys) ? "Il manque le champ prénom<br/>" : "";
+        $error .= !in_array("last_name", $keys) ? "Il manque le champ nom de famille<br/>" : "";
+        $error .= !in_array("cellphone", $keys) ? "Il manque le champ numéro de téléphone portable<br/>" : "";
+        $error .= !in_array("phone", $keys) ? "Il manque le champ numéro de téléphone fixe<br/>" : "";
+
+        $new_user_data = $request->request;
+
+        if ($error == "") {
+            $user->setAddress($new_user_data->get("address"));
+            $user->setEmail($new_user_data->get("email"));
+            $user->setFirstName($new_user_data->get("first_name"));
+            $user->setLastname($new_user_data->get("last_name"));
+            $user->setCellphone($new_user_data->get("cellphone"));
+            $user->setPhone($new_user_data->get("phone"));
+            if (in_array("is_admin", $keys) and $new_user_data->get("is_admin") == "on")
+                $user->setRoles(["ROLE_ADMIN"]);
+            $this->getDoctrine()->getManager()->persist($user);
+            $this->getDoctrine()->getManager()->flush();
+        }
+
+        return $this->user_edit($username, $error);
     }
 
     /**
