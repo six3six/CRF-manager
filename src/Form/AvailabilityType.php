37fdd04b3dc5a2a817eefa8bbfa80a5639c564d7
Louis DESPLANCHE <louis.desplanche@edu.esiee.fr>
2020-04-14 14:20:53 +0200

    Move availability to form

---
+++ src/Form/AvailabilityType.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Availability;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class AvailabilityType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $date_options = ["widget" => "single_text", 'attr' => ['class' => 'datepicker'], "format" => "dd/MM/yyyy HH:mm", "html5" => false];
+        $builder
+            ->add('start', DateTimeType::class, $date_options)
+            ->add('stop', DateTimeType::class, $date_options)
+            ->add('save', SubmitType::class);
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Availability::class,
+        ]);
+    }
+}
---
+++ src/Form/EventType.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Event;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class EventType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('start')
+            ->add('stop')
+            ->add('name')
+            ->add('created_by')
+            ->add('registered_users');
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Event::class,
+        ]);
+    }
+}
